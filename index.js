0221845363c4fef1e9e069985f9edb5487371eaf
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-03-31 17:11:02 -0400

    so far all test are still failing

---
+++ index.js
@@ -0,0 +1,28 @@
+const validatePassword = ([]) => {
+    let valid = []
+    let lowerCase = /[a - z]/
+    let upperCase = /[ A - Z]/
+    let num = /[0 - 9]/
+
+
+switch (valid.length <= 8 ){
+    case true:
+        break;
+}switch (valid == lowerCase){
+    case true:
+        break;
+}switch (valid == upperCase){
+    case true:
+        break;
+}switch (valid == num){
+    case true:
+        break;
+        default: false
+}
+
+    
+    
+
+
+}
+module.exports = validatePassword([])

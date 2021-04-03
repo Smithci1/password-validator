8096a021f8f40f4e45feb18f43acb6dda7f39726
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-03 13:14:06 -0400

    5 test now passing, but the first one is not.

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 let valid = true
+let invalid = false
+
 
 const passwordPattern = {
     charLength: function() {
         if (password.value.length() >= 8){
             return valid
-        } 
-    },
-    Case: function() {
-        if ((password.value.toLowerCase()) && (password.value.toUpperCase())) {
-            return valid    
+        } else if (password.value.length() < 9){
+            return invalid
         }
     },
+    upCase: function() {
+        if (password.value.toUpperCase()) {
+            return valid
+        }else if (!password.value.toUpperCase()){
+            return invalid
+        }
+    },
+    lowCase: function() {
+        if (password.value.toLowerCase()) {
+            return valid
+        }else if (!password.value.toLowerCase()){
+            return invalid
+        }
+    
+},
     num: function(){
         if (password.value === [0-9]){
             return valid
-        }    
+        } else if (password.value != [0-9]){
+            return invalid
+        }
     },
     specialChar: function(){
-        let sc = []
+        let sc = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        if (password.value === sc){
+            return valid
+        }else if  (password.value != sc){
+            return invalid
+        }
     }
 }
+const validatePassword = (password) => {  
+      
+ if (password === passwordPattern.specialChar && 
+    password === passwordPattern.num &&
+    password === passwordPattern.upCase &&
+    password === passwordPattern.lowCase &&
+    password === passwordPattern.charLength){
+        return valid
 
-const validatePassword = (password) => {
-    let lengthCase = ((passwordPattern.charLength) && (passwordPattern.Case))  
-    let numSc = ((passwordPattern.num) && (passwordPattern.specialChar))
-
-    if (lengthCase && numSc) {
-        return true
-        
-    }else false
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    }else return invalid
+            
 }
 module.exports = validatePassword

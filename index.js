8ab4b0409570c03d0ce713ff7b5684160434e8f8
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-04 23:30:04 -0400

    found out an hour before that pretty much everything here is no good

--- index.js
+++ index.js
@@ -3,54 +3,85 @@ let invalid = false
 
 
 const passwordPattern = {
-    charLength: function() {
-        if (password.value.length() >= 8){
+    charLength: function(password) {
+        if (password.length() >= 8){
             return valid
-        } else if (password.value.length() < 9){
+        } else if (password.length() < 8){
             return invalid
         }
     },
-    upCase: function() {
-        if (password.value.toUpperCase()) {
+    upCase: function(password) {
+        if (password.toUpperCase()) {
             return valid
-        }else if (!password.value.toUpperCase()){
+        }else if (password.value.toUpperCase() != password.value){
             return invalid
         }
     },
-    lowCase: function() {
+    lowCase: function(password) {
         if (password.value.toLowerCase()) {
             return valid
-        }else if (!password.value.toLowerCase()){
+        }else if (password.value.toLowerCase() != password.value){
             return invalid
         }
     
 },
-    num: function(){
-        if (password.value === [0-9]){
-            return valid
-        } else if (password.value != [0-9]){
-            return invalid
+    num: function(password){
+        let numbers = [0,1,2,3,4,5,6,7,8,9]
+        for (i = 0; i < numbers.length; i++){
+            if(password.indexof(password[i]) >= 0){
+                return valid
+            }
         }
     },
-    specialChar: function(){
-        let sc = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-        if (password.value === sc){
+       
+    specialChar: function(password) {
+        let sc = ['!','@','#','$','%','^','&','?','~'] 
+          for (i = 0; i < sc.length; i++)
+        if (password.indexof(sc[i]) >= 0){
             return valid
-        }else if  (password.value != sc){
+        }else if  (password.value === !sc){
             return invalid
-        }
+        } 
     }
 }
-const validatePassword = (password) => {  
-      
- if (password === passwordPattern.specialChar && 
-    password === passwordPattern.num &&
-    password === passwordPattern.upCase &&
-    password === passwordPattern.lowCase &&
-    password === passwordPattern.charLength){
-        return valid
 
-    }else return invalid
+
+const validatePassword = (password) => {  
+ if (password === passwordPattern.num || 
+    password === passwordPattern.specialChar ||
+    password === passwordPattern.upCase ||
+    password === passwordPattern.lowCase ||
+    passwordPattern.charLength){
+        return valid
+    }else if (password != passwordPattern.num || 
+        password != passwordPattern.specialChar ||
+        password != passwordPattern.upCase ||
+        password != passwordPattern.lowCase ||
+        passwordPttern.charLength){
+            return invalid
+        }
+       
+    //if (password != passwordPattern.num && 
+      //  passwordPattern.specialChar &&
+        //passwordPattern.upCase &&
+        //passwordPattern.lowCase &&
+        //passwordPattern.charLength){
+          //  return invalid
+
+            //}
+
+      
             
-}
+       
+   
+    //else if (password === 
+    //passwordPattern.specialChar && 
+    //passwordPattern.num &&
+   // //passwordPattern.upCase &&
+     //passwordPattern.lowCase &&
+   // passwordPattern.charLength){ 
+            //return true
+
+      
+}    
 module.exports = validatePassword
